261cabbf190d6ee35e56a451091d58c07a64d713
M <turcotte.mike@gmail.com>
2017-10-11 17:34:05 -0400

    Added some logic to moves. Not sure if we will keep the Tiles Class within the Board class. Look eventually to optimize algorithm by adding a "mask" to the board that can keep track of the current state of the board + move list in order to determine if we've already taken this route (hinted by teacher)

--- src/Board.java
+++ src/Board.java
@@ -1,25 +1,40 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class defines a game board of 7x7 spaces that can either be filled or not. Some spaces can be blank, aka not playable
+ * while others will contain either 1 or 2 as information.
+ */
 public class Board {
 
-    List<Tile> tiles;
+    String[][] tiles;
 
     /**
      * Constructor
      */
     public Board() {
-
+        this.tiles = new String[7][7];
     }
 
     /**
-     * Runs through possible scenarios with current moveset
+     * Displays current board layout
+     */
+    public void displayBoard() {
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print(this.tiles[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Runs through possible scenarios with current move set
      * @return
      */
     public boolean verifySolved() {
 
-
-        if (getMoves(getEmptyTiles()).isEmpty()) {
+        if (getMoves().isEmpty()) {
 
         }
         return false;
@@ -31,44 +46,69 @@ public class Board {
 
     public List<Tile> getEmptyTiles() {
         List<Tile> emptyTiles = new ArrayList<Tile>();
-
         return emptyTiles;
     }
 
     /**
-     * Finds all valid moves with the current board layout.
-     * @return
+     * Finds all valid moves within the current board layout. Valid move: defined in the lab is moving a filled tile
+     * to an empty one by hoping over a filled one. This unfills the tile that has been hopped over.
+     *
+     * @return getMoves : list of moves
      */
-    public List<Move> getMoves(List<Tile> emptyTiles) {
+    public List<Move> getMoves() {
         List<Move> moveList = new ArrayList<Move>();
 
 
         return moveList;
     }
 
-    public void setLayout(String[][] boardLayout) {
+    /**
+     * Sets this board's layout
+     * @param boardLayout
+     */
+    public void setBoard(String[][] boardLayout) {
+        System.arraycopy(boardLayout, 0, this.tiles, 0, boardLayout.length);
+    }
 
+    public void makeMove(Move move) {
+        this.tiles[move.fromPositionX][move.fromPositionY] = "1";
+        this.tiles[move.toPositionX][move.toPositionY] = "2";
     }
 
     /**
-     * This class records a move between two Tiles on this specific board.
+     * This class defines a move between two positions on this board.
      */
     class Move {
+
+        int fromPositionX;
+        int fromPositionY;
+        int toPositionX;
+        int toPositionY;
 
         /**
          * Constructor
          */
-        public Move() {
+        public Move(int[] fromPosition, int[] toPosition) {
+            fromPositionX = fromPosition[0];
+            fromPositionY = fromPosition[1];
 
+            toPositionX = toPosition[0];
+            toPositionY = toPosition[1];
         }
     }
 
     /**
      * This class defines a tile on this board. Can be either filled or not.
+     *
+     * WARNING: NOT SURE IF WILL BE FINALLY IMPLEMENTED. I checked with the teacher, he said maybe a 2d
+     * array would be less complicated (don't have to worry about literal edge cases, if we go the route of
+     * linked list with the tiles)
      */
     class Tile {
 
         private boolean isFilled = true;
+        private int x;
+        private int y;
 
         /**
          * Constructor
@@ -89,6 +129,16 @@ public class Board {
          */
         public void fill() {
             isFilled = true;
+        }
+
+        /**
+         * Sets the position for a tile
+         * @param x : row tile position
+         * @param y : column tile position
+         */
+        public void setPosition(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,5 +1,15 @@
+import java.io.IOException;
+
 public class Puzzle {
-    public static void main (String[] args) {
+
+    public static void main (String[] args) throws IOException {
+
+        FileReader fr = new FileReader();
+        String[][] fileArray = fr.fileLinesToCharacterTable(fr.readSmallTextFile("puzzle_files/test.puzzle"));
+
+        Board puzzleBoard = new Board();
+        puzzleBoard.setBoard(fileArray);
+        puzzleBoard.displayBoard();
 
     }
 }
